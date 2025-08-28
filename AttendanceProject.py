5be3ddda19d2d0d7f72a885b8d40f677fa95d46f
MdeMathewSecundaria <cmathiasor26@gmail.com>
2025-08-28 21:23:29 +0100

    Añadí funcionalidad de guardar matches de las caras en un archivo .csv, como una lista de accesos

--- AttendanceProject.py
+++ AttendanceProject.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 import face_recognition
 import os
+from datetime import datetime
 
 # Find images and create a list for images and their names
 path = 'ImagesAttendance/'
@@ -23,6 +24,21 @@ def findEncodings(images_list):
         encodeList.append(face_recognition.face_encodings(image)[0])
 
     return encodeList
+
+def markAttendance(name):
+    with open('AttendanceProject/Attendance.csv', 'r+') as f:
+        myDataList = f.readlines()
+        nameList = []
+        for line in myDataList:
+            entry = line.split(',')
+            nameList.append(entry[0])
+        if name not in nameList:
+            time = datetime.now().strftime('%H:%M:%S')
+            f.writelines( f'\n{name},{time}')
+
+    return
+
+
 
 encodeListKnown = findEncodings(imagesList)
 print("Encoding completed")
@@ -52,6 +68,7 @@ while(True):
             cv2.rectangle(imageCam, (x1, y1), (x2, y2), (0, 255, 0), 2)
             cv2.rectangle(imageCam, (x1,y2-35), (x2, y2), (0, 255, 0), cv2.FILLED)
             cv2.putText(imageCam,name, (x1+6,y2-6), cv2.FONT_HERSHEY_COMPLEX, 1, (255,255,255), 2)
+            markAttendance(name)
 
     cv2.imshow('Video', imageCam)
     cv2.waitKey(1)
